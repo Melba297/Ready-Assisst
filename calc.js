499c64ba2f811bd2f3542504c5fbd086ddc3a735
Melba297 <64897664+Melba297@users.noreply.github.com>
2020-05-09 19:05:56 +0530

    Calculator in JS

---
+++ calc.js
@@ -0,0 +1,37 @@
+console.log("create caluclator")
+function add(n1,n2) {
+    console.log(n1+n2)
+
+        
+}
+ add(1,2)   
+
+function sub(n1,n2) {
+    console.log(n1-n2)
+
+}
+
+sub(1,2)
+
+function mulp(n1,n2) {
+    console.log(n1*n2)
+}
+
+mulp(1,2)
+
+function div(n1,n2) {
+    console.log(n1/n2)
+}
+
+div(1,3)
+
+
+
+
+
+
+
+
+
+
+
